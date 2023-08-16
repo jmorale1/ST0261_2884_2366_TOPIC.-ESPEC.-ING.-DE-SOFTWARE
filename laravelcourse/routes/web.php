5ffc0b8a3241694a4dce037a7f3a3131883b3732
jmorale1 <124903812+jmorale1@users.noreply.github.com>
2023-08-16 09:20:52 -0500

    trabajoenclase2

--- laravelcourse/routes/web.php
+++ laravelcourse/routes/web.php
@@ -15,9 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 /*Route::get('/', function () {
     return view('welcome');
-});*/
+});
 
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
 Route::get('/about', function () {
     $data1 = "About us - Online Store";
     $data2 = "About us";
@@ -28,7 +27,14 @@ Route::get('/about', function () {
       ->with("description", $description)
       ->with("author", $author);
 })->name("home.about");
-Route::get('/products', 'App\Http\Controllers\ProductController@index')->name("product.index");
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name("product.create");
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name("product.save");
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name("product.show");
+
+*/
+
+Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index');
+Route::get('/about', 'App\Http\Controllers\AboutController@index')->name('about.index');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+//que me devuelva la vista, ejemplo los http//localhost:8000/products/1
